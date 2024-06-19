d3e4525ad973302798013b993d73fd59d48f40cf
NicolasRodriguez115 <nr115935@gmail.com>
2024-06-19 18:56:26 -0400

    Finished 2.1

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 our_routes = {"LAX", "JFK", "CDG", "DXB"}
 competitor_routes = {"JFK", "CDG", "LHR", "BKK"}
-
+customer_ids = ["C001", "C002", "C003", "C002", "C001", "C004"]
 def main():
+    from flight_route_comparison import route_comparison
+    from unique_customer_ids import unique_ids
     # Lesson 3: Assignments | Sets
     # 1. Python Sets Adventure
-    # ask 1: Flight Route Comparison
-    
-
+    route_comparison()
+    # Task 1: Flight Route Comparison
+    unique_ids()
 
 if __name__ == "__main__":
     main()
---
+++ unique_customer_ids.py
@@ -0,0 +1,4 @@
+from main import customer_ids
+def unique_ids():
+    remove_duplicates = set(customer_ids)
+    print(remove_duplicates)
